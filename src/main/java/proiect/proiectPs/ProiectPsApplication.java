98177626bfc47d4983095580f456972ce9494b70
matei667 <popoi.matei@gmail.com>
2025-03-17 15:07:22 +0200

    commit

--- src/main/java/proiect/proiectPs/ProiectPsApplication.java
+++ src/main/java/proiect/proiectPs/ProiectPsApplication.java
@@ -3,11 +3,14 @@ package proiect.proiectPs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class ProiectPsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProiectPsApplication.class, args);
 	}
-
 }
+
