77b1dc6526d33ac72356fcc044216368cf7c7339
Alexandru Miulescu <alexandru030417@yahoo.com>
2025-05-25 00:59:20 +0300

    stersfisierredundant login/registerworks+corsfixes

--- src/frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser"
-import { AppComponent } from "./app/app.component"
-import { appConfig } from "./app/app.config"
-
-// Remove any CSP meta tags before bootstrapping
-const cspMetaTags = document.querySelectorAll('meta[http-equiv="Content-Security-Policy"]')
-cspMetaTags.forEach((tag) => tag.remove())
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
